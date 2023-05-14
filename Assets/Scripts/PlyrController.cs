fdc0c18656a71d5d5aeca7e3db05d8d352070d2a
Hasan Arnavutoğlu <hasanversion2009@gmail.com>
2023-05-14 23:06:18 +0300

    fix png

--- Assets/Scripts/PlyrController.cs
+++ Assets/Scripts/PlyrController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.PlayerSettings;
+
 
 public class PlyrController : MonoBehaviour
 {
